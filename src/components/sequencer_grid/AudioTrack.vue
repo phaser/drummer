4d57dcde699f436a0f3b192d035e513a46e32926
Cristian Bidea <cristian.bidea@gmail.com>
2019-12-25 18:53:17 +0200

    Demo playing considering BPM

--- src/components/sequencer_grid/AudioTrack.vue
+++ src/components/sequencer_grid/AudioTrack.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="container">
-    <audio :src="audio_file" ref="audio_tag"></audio>
+    <audio :src="audio_file" ref="audio_tag0"></audio>
+    <audio :src="audio_file" ref="audio_tag1"></audio>
+    <audio :src="audio_file" ref="audio_tag2"></audio>
+    <audio :src="audio_file" ref="audio_tag3"></audio>
     <div class="row">
       <span class="badge badge-success" style="height: 40px; width: 100px">
         <br />
@@ -14,6 +17,10 @@
         :ref="'cell' + item"
         @click="clickCell('cell' + item)"
       >o</span>
+    </div>
+    <div class="row">
+      <button class="btn btn-primary" @click="play">Play</button>
+      <button class="btn btn-primary" @click="stop">Stop</button>
     </div>
   </div>
 </template>
@@ -36,7 +43,8 @@ export default {
   },
   data() {
     return {
-      cell_status: {}
+      cell_status: {},
+      playInterval: null
     };
   },
   methods: {
@@ -44,7 +52,36 @@ export default {
       // this.$refs.audio_tag.play();
       // eslint-disable-next-line no-console
       console.log(this.$refs);
-      this.$refs.cell10[0].className = "badge badge-primary";
+      let bpm = 400 / 60;
+      let tick = 0;
+      let interval = 1.0 / bpm;
+      let auclip0 = this.$refs.audio_tag0;
+      let auclip1 = this.$refs.audio_tag1;
+      let auclip2 = this.$refs.audio_tag2;
+      let auclip3 = this.$refs.audio_tag3;
+      this.playInterval = setInterval(() => {
+        // eslint-disable-next-line no-console
+        console.log("Tick: " + tick);
+
+        if (this.cell_status["cell" + tick] == true) {
+          if (!(auclip0.duration > 0 && !auclip0.paused)) {
+            auclip0.play();
+          } else if (!(auclip1.duration > 0 && !auclip1.paused)) {
+            auclip1.play();
+          } else if (!(auclip2.duration > 0 && !auclip2.paused)) {
+            auclip2.play();
+          } else if (!(auclip3.duration > 0 && !auclip3.paused)) {
+            auclip3.play();
+          }
+        }
+        tick++;
+        tick = tick % this.grid_length;
+      }, interval * 1000);
+      // eslint-disable-next-line no-console
+      console.log("Interval: " + interval);
+    },
+    stop() {
+      clearInterval(this.playInterval);
     },
     clickCell(cellRef) {
       this.cell_status[cellRef] =
